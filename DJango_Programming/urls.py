0a12bcd5b568ada934bc033b46704b31e5a023a1
poinguinie <wlgh0206@gmail.com>
2021-08-05 02:29:13 +0900

    포스트 목록, 상세 페이지에 대표 이미지 출력하기

--- DJango_Programming/urls.py
+++ DJango_Programming/urls.py
@@ -17,9 +17,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include #=> 교재 표기 x
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('blog/', include('blog.urls')),
     # 교재 표기 : path('blog', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('', include('single_pages.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
