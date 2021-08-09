b5e06b91d10aa8d33bcd41dfa6996d4f4e594f70
poinguinie <wlgh0206@gmail.com>
2021-08-09 11:42:27 +0900

    태그 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
-    path('category/<str:slug>/', views.category_page)
+    path('category/<str:slug>/', views.category_page),
+    path('tag/<str:slug>/', views.tag_page),
     # path('<int:pk>/', views.single_post_page),
     # path('', views.index),
 ]
