b5b468095328b6f25ff2f75dd4e3f11c83d125b3
poinguinie <wlgh0206@gmail.com>
2021-08-17 16:37:45 +0900

    포스트 수정 기능 추가하기

--- blog/tests.py
+++ blog/tests.py
@@ -105,7 +105,47 @@ class TestView(TestCase):
         self.assertEqual(last_post.title, "Post Form 만들기")
         self.assertEqual(last_post.author.username, 'john')
 
+    def test_update_post(self):
+        update_post_url = f'/blog/update_post/{self.post_003.pk}/'
 
+        # 로그인을 하지 않은 경우
+        response = self.client.get(update_post_url)
+        self.assertNotEqual(response.status_code, 200)
+
+        # 로그인은 했지만 작성자가 아닌 경우
+        self.assertNotEqual(self.post_003.author, self.user_Jack)
+        self.client.login(
+            username=self.user_Jack.username,
+            password='jack1355!'
+        )
+        response = self.client.get(update_post_url)
+        self.assertEqual(response.status_code, 403)
+
+        # 작성자(John)가 접근하는 경우
+        self.client.login(
+            username=self.post_003.author.username,
+            password='john1355!'
+        )
+        soup = self.connectSoup(update_post_url)
+
+        self.assertEqual('Edit Post - Blog', soup.title.text)
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('Edit Post', main_area.text)
+
+        response = self.client.post(
+            update_post_url,
+            {
+                'title': '세 번째 포스트를 수정했습니다.',
+                'content': '안녕? 만나서 반가워! 이 포스트는 수정됐어!',
+                'category': self.category_music.pk
+            },
+            follow=True
+        )
+        soup = BeautifulSoup(response.content, 'html.parser')
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('세 번째 포스트를 수정했습니다.', main_area.text)
+        self.assertIn('안녕? 만나서 반가워! 이 포스트는 수정됐어!', main_area.text)
+        self.assertIn(self.category_music.name, main_area.text)
 
     def navbar_test(self, soup):
         navbar = soup.nav
