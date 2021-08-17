9b9475136ae30fa060f10bad4bc959e415b127e7
poinguinie <wlgh0206@gmail.com>
2021-08-17 18:28:18 +0900

    Content 입력란에 마크다운 적용하기

--- DJango_Programming/urls.py
+++ DJango_Programming/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     # 교재 표기 : path('blog', include('blog.urls')),
     path('admin/', admin.site.urls),
+    path('markdownx/',include('markdownx.urls')),
     path('', include('single_pages.urls'))
 ]
 
