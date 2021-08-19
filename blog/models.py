c8d826e470bfd367597e21d1a9aa4ccbdb896566
poinguinie <wlgh0206@gmail.com>
2021-08-19 20:32:18 +0900

    대문 페이지 수정하기

--- blog/models.py
+++ blog/models.py
@@ -59,6 +59,14 @@ class Post(models.Model):
     def get_content_markdown(self):
         return markdown(self.content)
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://ui-avatars.com/api/?name={self.author.username}'
+            # return f'https://doitdjango.com/avatar/id/270/8f91e568ddb70d3e/svg/{self.author.email}'
+
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -76,4 +84,5 @@ class Comment(models.Model):
         if self.author.socialaccount_set.exists():
             return self.author.socialaccount_set.first().get_avatar_url()
         else:
-            return f'https://doitdjango.com/avatar/id/270/8f91e568ddb70d3e/svg/{self.author.email}'
+            return f'https://ui-avatars.com/api/?name={self.author.username}'
+            # return f'https://doitdjango.com/avatar/id/270/8f91e568ddb70d3e/svg/{self.author.email}'
