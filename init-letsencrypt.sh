0351a12a8741ea65d1e2d2a11de28c1696cecb36
Poinguinie <64086893+poinguinie@users.noreply.github.com>
2021-08-21 19:25:10 +0900

    Update init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(poinguinie-django.shop www.poinguinie-django.shop)
+domains=(poinguinie-django.com www.poinguinie-django.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
